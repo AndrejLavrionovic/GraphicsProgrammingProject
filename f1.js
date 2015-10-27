64b4de4ff26bc9ff79881a90814e9ee22f23cbce
Andrej Lavrinovic <Andrej Lavrinovic>
2015-10-27 17:38:53 +0000

    Starts project F1 - game. Created .html, .js, and .css

---
+++ f1.js
@@ -0,0 +1,8 @@
+/***********************************************************************************
+* This is JavaScript file that sets the canvas element.
+***********************************************************************************/
+
+// Creates variable that is pointing on canvas element
+var can = document.getElementById("canvas");
+can.width = 400;
+can.height = 600;
